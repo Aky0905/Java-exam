f6e6a91333d5141ad9a2085959d74430f647d69c
Aky0905 <wonder090517@gmail.com>
2024-12-26 18:22:42 +0900

    GUI 생성

---
+++ timetable.java
@@ -0,0 +1,7 @@
+public class Timetable {
+    private JFrame frame;
+    private JTable table;
+    private DefaultTableModel tableModel;
+    private JButton removeButton;
+    private JButton saveButton;
+    private JButton loadButton;
